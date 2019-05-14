654902b8562d8a49c879ade3e92a3ad6cff263ec
Huy Pham <p.q.huy97@gmail.com>
2019-05-14 21:54:21 +0700

    Add host server

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-#!/bin/env python
+import os
+
 from app import create_app, socketio
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    port = int(os.getenv('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
